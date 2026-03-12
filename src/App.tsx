7e091e4c2cd8274c1f808d9fa25a062fc5bf3a64
xingchui <273109504@qq.com>
2026-03-12 15:52:18 +0800

    fix: add multiple debug spheres to verify orthographic projection

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,8 @@
-import { Canvas, useThree } from '@react-three/fiber'
+import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
-import { useState, useRef, useCallback, useEffect } from 'react'
+import { useState, useRef, useCallback } from 'react'
 import * as THREE from 'three'
 import { c60Data } from './data/c60'
-
-// Component to verify camera type and log debug info
-function CameraDebug() {
-  const { camera } = useThree()
-  
-  useEffect(() => {
-    console.log('Camera type:', camera.type)
-    console.log('Is OrthographicCamera:', camera instanceof THREE.OrthographicCamera)
-    console.log('Is PerspectiveCamera:', camera instanceof THREE.PerspectiveCamera)
-    
-    if (camera instanceof THREE.OrthographicCamera) {
-      console.log('Orthographic camera properties:', {
-        left: camera.left,
-        right: camera.right,
-        top: camera.top,
-        bottom: camera.bottom,
-        near: camera.near,
-        far: camera.far,
-        position: camera.position.toArray()
-      })
-    }
-  }, [camera])
-  
-  return null
-}
 import { c70Data } from './data/c70'
 import { c20Data } from './data/c20'
 import { c76Data } from './data/c76'
@@ -347,8 +322,6 @@ function App() {
             camera={{ position: [0, 0, 12], fov: 60 }}
             gl={{ antialias: true }}
           >
-            <CameraDebug />
-            
             {/* 添加背景色 */}
             <color attach="background" args={['#F8F9FA']} />
             
@@ -371,7 +344,6 @@ function App() {
           <Canvas 
             gl={{ antialias: true }}
           >
-            <CameraDebug />
             
             {/* Add orthographic camera */}
             <orthographicCamera 
@@ -387,10 +359,18 @@ function App() {
             <pointLight position={[10, 10, 10]} intensity={1} color="#ffffff" />
             <pointLight position={[-10, -10, -10]} intensity={0.3} color="#b8c5d6" />
             
-            {/* Debug: Red sphere to verify orthographic rendering */}
-            <mesh position={[0, 0, 0]}>
-              <sphereGeometry args={[2, 32, 32]} />
+            {/* Debug: Multiple spheres at different depths to verify orthographic rendering */}
+            <mesh position={[0, 0, -5]}>
+              <sphereGeometry args={[1.5, 32, 32]} />
               <meshBasicMaterial color="red" />
+            </mesh>
+            <mesh position={[0, 0, 0]}>
+              <sphereGeometry args={[1.5, 32, 32]} />
+              <meshBasicMaterial color="green" />
+            </mesh>
+            <mesh position={[0, 0, 5]}>
+              <sphereGeometry args={[1.5, 32, 32]} />
+              <meshBasicMaterial color="blue" />
             </mesh>
             
             <MoleculeScene 
