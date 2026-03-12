64752a361a82f97b391f00632598fc4b368f4b73
xingchui <273109504@qq.com>
2026-03-12 13:59:59 +0800

    fix: use orthographic prop with camera params on Canvas

--- src/App.tsx
+++ src/App.tsx
@@ -342,14 +342,18 @@ function App() {
           </Canvas>
         ) : (
           <Canvas 
+            camera={{
+              position: [0, 0, 12],
+              left: -15,
+              right: 15,
+              top: 15,
+              bottom: -15,
+              near: 0.1,
+              far: 1000
+            }}
+            orthographic
             gl={{ antialias: true }}
           >
-            {/* Orthographic Camera */}
-            <orthographicCamera 
-              position={[0, 0, 12]}
-              args={[-15, 15, 15, -15, 0.1, 1000]}
-            />
-            
             {/* 添加背景色 */}
             <color attach="background" args={['#F8F9FA']} />
             
